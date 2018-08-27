0bb8c405df50621f71134bbeb1db60b1a27e1f1f
Casey Poier <cpoier@c02w90v7htdg.reicorpnet.com>
2018-08-27 10:16:18 -0700

    added third party json server to use for db testing

--- schema/schema.js
+++ schema/schema.js
@@ -9,7 +9,11 @@ const {
 
 const users = [
     { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Samantha', age: 21 }
+    { id: '47', firstName: 'Samantha', age: 21 },
+    { id: '42', firstName: 'frank', age: 22 },
+    { id: '37', firstName: 'Sant', age: 41 },
+    { id: '57', firstName: 'Ssetha', age: 51 },
+    { id: '87', firstName: 'Jon', age: 66 }
 ]
 
 const UserType = new GraphQLObjectType({
@@ -21,8 +25,8 @@ const UserType = new GraphQLObjectType({
     }
 });
 
-const RootQueryType = new GraphQLObjectType({
-    name: 'RootQuery',
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
     fields: {
         user: {
             type: UserType,
@@ -35,5 +39,5 @@ const RootQueryType = new GraphQLObjectType({
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQueryType,
+    query: RootQuery,
 });
